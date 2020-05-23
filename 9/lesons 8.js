0e5935adb0c837440c94013a568fd927267a00af
1troya129 <boginskyal@gmail.com>
2020-05-23 14:28:32 +0700

    9 новые

--- 9/lesons 8.js
+++ 9/lesons 8.js
@@ -2,11 +2,9 @@ const fs = require("fs");
 const readlineSync = require("readline-sync");
 let todos = JSON.parse(fs.readFileSync("todos.json"));
 let x = 0;
-let index = 0;
 for (let i = 0; i < todos.length; ++i) {
     if (todos[i].completed === true) {
-        index=i;
-        todos.splice(index);
+        todos.splice(i, 1);
         x=x+1; 
     } 
 } console.log(x+" "+"todos removed");
